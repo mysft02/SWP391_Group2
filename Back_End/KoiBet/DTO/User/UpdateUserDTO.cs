98247c78f26ce0f094d09bac7d4a504878b9f8bd
DressAnDy <mnhat2232004@gmail.com>
2024-10-04 00:37:47 +0700

    4-10-2024
    CRUD of users

---
+++ Back_End/KoiBet/DTO/User/UpdateUserDTO.cs
@@ -0,0 +1,15 @@
+﻿namespace KoiBet.DTO.User
+{
+    public class UpdateUserDTO
+    {
+        public string users_id { get; set; }
+
+       public string? full_name { get; set; } = string.Empty.ToString();
+
+        public string? email { get; set; } = string.Empty.ToString();
+
+       public string? phone { get; set; } = string.Empty.ToString();
+
+       public string? role_id { get; set; } = string.Empty.ToString();
+    }
+}
