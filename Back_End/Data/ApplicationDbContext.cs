c07967d9ed047ab326adcc6e3d3f7f0e6a258018
DressAnDy <mnhat2232004@gmail.com>
2024-10-09 02:31:35 +0700

    Adding KoiStandard Service
    Not Test Yet!!!

--- Back_End/Data/ApplicationDbContext.cs
+++ Back_End/Data/ApplicationDbContext.cs
@@ -12,6 +12,8 @@ namespace KoiBet.Data
         public DbSet<Users> Users { get; set; }
         public DbSet<Roles> Roles { get; set; }
         public DbSet<FishKoi> FishKoi { get; set; }
+        public DbSet<KoiStandard> KoiStandards { get; set; }
+
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
